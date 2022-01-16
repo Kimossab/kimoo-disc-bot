5f8cb018556da92a19d362d52bbe4a1fc3f83c4b
Kimossab <kimossab@outlook.pt>
2022-01-16 19:44:25 +0000

    update packages

--- src/helper/tests.ts
+++ src/helper/tests.ts
@@ -1,34 +1,30 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
-/**
- * Connect to mock memory db.
- */
-export const connect =
-  async (): Promise<MongoMemoryServer> => {
-    const mongod = await MongoMemoryServer.create();
-    const uri = mongod.getUri();
+export class TestDB {
+  public static connect =
+    async (): Promise<MongoMemoryServer> => {
+      const mongod = await MongoMemoryServer.create();
+      const uri = mongod.getUri();
 
-    await mongoose.connect(uri);
-    return mongod;
+      await mongoose.connect(uri);
+      return mongod;
+    };
+
+  public static closeDatabase = async (
+    mongod: MongoMemoryServer
+  ): Promise<void> => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
   };
 
-/**
- * Close db connection
- */
-export const closeDatabase = async (
-  mongod: MongoMemoryServer
-): Promise<void> => {
-  await mongoose.connection.dropDatabase();
-  await mongoose.connection.close();
-  await mongod.stop();
-};
+  public static clearDatabase = async (): Promise<void> => {
+    const collections = mongoose.connection.collections;
 
-export const clearDatabase = async (): Promise<void> => {
-  const collections = mongoose.connection.collections;
-
-  for (const key in collections) {
-    const collection = collections[key];
-    await collection.deleteMany({});
-  }
-};
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
+    }
+  };
+}
